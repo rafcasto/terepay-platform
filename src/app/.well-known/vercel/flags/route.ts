aa9a7c461177f016c8bff6085464b75a432203bf
Rafael Castillo <rafcasto@gmail.com>
2026-03-23 22:02:09 +1300

    fixing invalid wellknown flag

--- src/app/.well-known/vercel/flags/route.ts
+++ src/app/.well-known/vercel/flags/route.ts
@@ -1,10 +1,24 @@
 import { getProviderData } from '@vercel/flags/next';
-import { NextResponse } from 'next/server';
-import * as flags from '@/lib/flags/flags';
+import { NextRequest, NextResponse } from 'next/server';
+import {
+  newApplicantDashboard,
+  paymentTrackingV2,
+  autoUnderwriting,
+  disableSmsOtp,
+} from '../../../../lib/flags/flags';
 
 export const runtime = 'edge';
 
-export async function GET() {
-  const data = await getProviderData(flags);
+export async function GET(request: NextRequest) {
+  const access = request.headers.get('Authorization');
+  if (access !== `Bearer ${process.env.FLAGS_SECRET}`) {
+    return NextResponse.json(null, { status: 401 });
+  }
+  const data = await getProviderData({
+    newApplicantDashboard,
+    paymentTrackingV2,
+    autoUnderwriting,
+    disableSmsOtp,
+  });
   return NextResponse.json(data);
 }
